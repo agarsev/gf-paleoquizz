64e25f0cf12ef5a3ea16a900999b17bdc2e1f55c
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2015-07-13 14:28:07 +0200

    Multi-question quiz

--- paleo.sh
+++ paleo.sh
@@ -37,6 +37,13 @@ get_semtree() {
     }'))
 }
 
+add_random_answer() {
+    gf "gr $2 $3 $4 $5 | lin -lang=$1"
+    get_output
+    flush
+    answers=$answers$'\n'$output
+}
+
 # START
 
 flush
@@ -72,28 +79,41 @@ while read line; do
                 echo "${semtree[2]};${semtree[3]};${semtree[1]}" >>$DB_FILE
             fi
         else
-            echo -e "\e[91mERROR\e[0m";
+            echo -e "\e[91mERROR\e[0m: sentence not understood";
         fi
     ;;
     quiz)
-        action=$(sort -R $DB_FILE | head -1)
-        IFS=';' sem=($action)
-        gf "lin -lang=Spa TimeFocus ${sem[1]} ${sem[0]} ${sem[2]}"
-        get_output
-        flush
-        echo "a) $output"
-        gf "gr TimeFocus ${sem[1]} ${sem[0]} ? | lin -lang=Spa"
-        get_output
-        flush
-        echo "b) $output"
-        gf "gr TimeFocus ${sem[1]} ${sem[0]} ? | lin -lang=Spa"
-        get_output
-        flush
-        echo "c) $output"
-        gf "gr TimeFocus ${sem[1]} ${sem[0]} ? | lin -lang=Spa"
-        get_output
-        flush
-        echo "d) $output"
+        points=0
+        if [[ $args == 'Spa' ]]; then
+            lang=Spa
+            prompt="Elige la opción correcta:"
+        else
+            lang=Eng
+            prompt="Choose the correct option:"
+        fi
+        facts=$(sort -R $DB_FILE | head -10)
+        IFS=$'\n'
+        for action in $facts; do
+            echo $prompt
+            IFS=';' sem=($action)
+            gf "lin -lang=$lang TimeFocus ${sem[1]} ${sem[0]} ${sem[2]}"
+            get_output
+            flush
+            correct=$output
+            answers=$correct
+            add_random_answer $lang TimeFocus ${sem[1]} ${sem[0]} ?
+            add_random_answer $lang TimeFocus ${sem[1]} ${sem[0]} ?
+            add_random_answer $lang TimeFocus ${sem[1]} ${sem[0]} ?
+            answers=$(sort -R <<<$answers)
+            echo $answers | paste <(echo -n $'1)\n2)\n3)\n4)') - -d' '
+            IFS=$'\n' answers=($answers)
+            echo -n ": "
+            read -r ans
+            if [[ ${answers[$((ans-1))]} == $correct ]]; then
+                points=$((points+1))
+            fi
+        done
+        echo "points: $points"
     ;;
     esac
 
